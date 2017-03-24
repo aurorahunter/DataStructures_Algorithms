8a9ff63315512eca6cb395f2fdedb457b1391331
varsha <varshapartha89@gmail.com>
2017-03-23 23:55:35 -0700

    wild card matching

---
+++ Dynamic_Programming/leetcode/WildCardMatching.java
@@ -0,0 +1,62 @@
+/*
+https://leetcode.com/problems/wildcard-matching/#/description
+Implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "*") → true
+isMatch("aa", "a*") → true
+isMatch("ab", "?*") → true
+isMatch("aab", "c*a*b") → false
+
+Time/Space Complexity: O(mxn)
+**/
+public class WildCardMatching {
+    public boolean isMatch(String s, String p) {
+        char[] str = s.toCharArray();
+        char[] pattern = p.toCharArray();
+
+        //replace multiple * with one *
+        //e.g a**b***c --> a*b*c
+        int writeIndex = 0;
+        boolean isFirst = true;
+        for ( int i = 0 ; i < pattern.length; i++) {
+            if (pattern[i] == '*') {
+                if (isFirst) {
+                    pattern[writeIndex++] = pattern[i];
+                    isFirst = false;
+                }
+            } else {
+                pattern[writeIndex++] = pattern[i];
+                isFirst = true;
+            }
+        }
+        boolean T[][] = new boolean[str.length + 1][writeIndex + 1];
+        if (writeIndex > 0 && pattern[0] == '*') {
+            T[0][1] = true;
+        }
+
+        T[0][0] = true;
+
+        for (int i = 1; i < T.length; i++) {
+            for (int j = 1; j < T[0].length; j++) {
+                if (pattern[j-1] == '?' || str[i-1] == pattern[j-1]) {
+                    T[i][j] = T[i-1][j-1];
+                } else if (pattern[j-1] == '*'){
+                    T[i][j] = T[i-1][j] || T[i][j-1];
+                }
+            }
+        }
+        return T[str.length][writeIndex];
+    }
+}
