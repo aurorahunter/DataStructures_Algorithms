938fc9c66278ea488c0bcb9b06b0e05c9c435d08
varsha <varshapartha89@gmail.com>
2017-03-24 00:01:26 -0700

    regex DP hard

---
+++ Dynamic_Programming/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,53 @@
+/*
+
+https://leetcode.com/problems/regular-expression-matching/#/description
+
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+**/
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        char[] text = s.toCharArray();
+        char[] pattern = p.toCharArray();
+        boolean T[][] = new boolean[text.length + 1][pattern.length + 1];
+
+        T[0][0] = true;
+        //Deals with patterns like a* or a*b* or a*b*c*
+        for (int i = 1; i < T[0].length; i++) {
+            if (pattern[i-1] == '*') {
+                T[0][i] = T[0][i - 2];
+            }
+        }
+        for (int i = 1; i < T.length; i++) {
+            for (int j = 1; j < T[0].length; j++) {
+                if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1]) {
+                    T[i][j] = T[i-1][j-1];
+                } else if (pattern[j - 1] == '*')  {
+                    T[i][j] = T[i][j - 2];
+                    if (pattern[j-2] == '.' || pattern[j - 2] == text[i - 1]) {
+                        T[i][j] = T[i][j] | T[i - 1][j];
+                    }
+                } else {
+                    T[i][j] = false;
+                }
+            }
+        }
+        return T[text.length][pattern.length]; 
+    }
+}
